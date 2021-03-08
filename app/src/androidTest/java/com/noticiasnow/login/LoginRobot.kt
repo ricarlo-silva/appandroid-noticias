1b2a2a827da2a4b966b66c1078355340cab4e745
Ricarlo Silva <>
2021-03-08 00:21:45 -0300

    fix: code smell

--- app/src/androidTest/java/com/noticiasnow/login/LoginRobot.kt
+++ app/src/androidTest/java/com/noticiasnow/login/LoginRobot.kt
@@ -6,7 +6,11 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.hasImeAction
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withHint
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.noticiasnow.R
 import org.hamcrest.Matchers.not
 
